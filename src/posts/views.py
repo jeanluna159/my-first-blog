689eb87d321a0df67889e4f158c4086bff3e605c
jeanluna <josenuevomundo55@gmail.com>
2023-07-09 17:31:49 -0500

    Mi aplicaion Django Girls

---
+++ src/posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.http import HttpResponse
+
+def post_list(request):
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+    return render(request, 'posts/post_list.html', {'posts': post})
+
+def post_detail(request, pk);
+    post = get_object_or_404(Post, pk=pk)
+    return HttpResponse(post)
